20dce594b2a478cc972695a7e0fa57f847c5d224
H3cth0r <hectorsucre13@gmail.com>
2023-09-19 13:56:46 -0600

    lol

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 const dev = process.argv.includes('dev');
 
@@ -9,6 +9,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
 	}
 };
 
