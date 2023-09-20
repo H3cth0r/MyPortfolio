3df1ec62cf7d175b992d324a5fa1586ee7e1a9a6
H3cth0r <hectorsucre13@gmail.com>
2023-09-19 18:14:25 -0600

    lola

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,5 @@
 import { getMyWorkData } from '$lib/server/database.js';
 
-/** @type {import('./$types').PageServerLoad} */
 export const load = () => {
   let resultData = getMyWorkData();
   console.log(resultData);
